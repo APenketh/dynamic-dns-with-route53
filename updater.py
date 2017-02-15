0f55d4387e06a11865b4e799a45266f931781a48
root <alex@apenketh.com>
2017-02-15 17:05:58 +0000

    Added Logging

--- updater.py
+++ updater.py
@@ -4,6 +4,8 @@ import requests
 import boto3
 import logging
 
+logging.basicConfig(filename='dnsupdater.log',level=logging.INFO)
+
 def getPublicIp():
 	siteList = ( 'http://myip.dnsomatic.com', 'http://canihazip.com/s' )	
 	siteIpReturn = ""
@@ -13,13 +15,14 @@ def getPublicIp():
 		if siteHolder.status_code == int(200):
 			if siteIpReturn == "":
 				siteIpReturn = siteHolder.text 
+				logging.info(" Site: '{0}' provided IP: '{1}'" .format(sites, siteHolder.text))
 			elif siteHolder.text == siteIpReturn:
 				siteIpReturn = siteHolder.text
+				logging.info(" Site: '{0}' provided IP: '{1}'" .format(sites, siteHolder.text))
 			else:
-				print("Replace With Logging2")
+				logging.warning(" Site: '{0}' provided IP address '{1}' which did not match the last provided IP address of '{2}', please investigate manually." .format(sites, siteHolder.text, siteIpReturn))
 		else:
-			print("Replace With Logging1")
+			logging.warning(" Site: '{0}' is not currently working." .format(sites))
 	return siteIpReturn
 
 externalIp = getPublicIp()
-print externalIp
