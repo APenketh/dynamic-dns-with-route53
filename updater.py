b2235b4729e7f8b369b44e189074fc6a25ae7e59
root <alex@apenketh.com>
2017-02-21 13:31:20 +0000

    Added the bulk of the code for the boto connection and the record updater.
    Its in a working state now but needs additional logging and error checking.

--- updater.py
+++ updater.py
@@ -1,12 +1,37 @@
 #!/usr/bin/env python
 
-import requests
-import boto3
-import logging
+import requests, boto3, logging
+from boto3.session import Session
 
 logging.basicConfig(filename='dnsupdater.log',level=logging.INFO)
 
+"""
+Add your information to the following variables
+$zoneName, $zoneId, $aws_access_key_id,
+$aws_secret_access_key
+"""
+zoneName = ""
+zoneId = ""
+
+def sessionDetails():
+	"""
+	Contains the IAM details to use for initating
+	a connection through the boto api
+	"""
+	session = Session(
+        	aws_access_key_id='',
+        	aws_secret_access_key='',
+        	region_name="eu-west-1",
+    	)
+	if session:
+        	return session
+
 def getPublicIp():
+	"""
+	Gets the clients public IP address from a number
+	of external services, it then checks the results
+	against each other for validity.
+	"""
 	siteList = ( 'http://myip.dnsomatic.com', 'http://canihazip.com/s' )	
 	siteIpReturn = ""
 
@@ -25,4 +50,42 @@ def getPublicIp():
 			logging.warning(" Site: '{0}' is not currently working." .format(sites))
 	return siteIpReturn
 
-externalIp = getPublicIp()
+def modify_record(client, zoneName, zoneId, externalIp, ttl=600):
+	"""
+	Modifies the zone with the given
+	infromation pased through.
+	"""
+	print client.change_resource_record_sets(
+        	HostedZoneId=zoneId,
+        	ChangeBatch={
+            		'Comment': 'Modified By Dynamic DNS Updater',
+            		'Changes': [
+                		{
+                    		'Action': 'UPSERT',
+                    		'ResourceRecordSet': {
+                        		'TTL': ttl,
+                        		'Name': zoneName,
+                        		'Type': 'A',
+                        		'ResourceRecords': [
+                            		{
+                                	'Value': externalIp
+                            		},
+                        		]
+                    		}
+                		},
+            		]
+        	}
+    	)
+
+if __name__ == '__main__':
+	"""
+	Set up the connection to Route53 through
+	boto, get the external IP address of the 
+	server and then initate a change.
+	"""
+	session = sessionDetails()
+	client = session.client('route53')
+	externalIp = getPublicIp()
+
+	logging.info("Updating Zone for {} with IP {}" .format(zoneName, externalIp))
+	modify_record(client, zoneName, zoneId, externalIp)
